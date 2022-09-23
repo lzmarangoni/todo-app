7a00bf5e3e46c6c776b0092cf2e6898413d76938
Luiz Marangoni <lzmarangoni@gmail.com>
2022-09-22 22:13:21 -0300

    lista de tarefas estilizada

---
+++ js/todoApp.js
@@ -0,0 +1,8 @@
+const task = document.getElementById('todoInput');
+const taskValue = task.value;
+const addTask = document.querySelector('.todoCheck');
+
+
+addTask.addEventListener('click', ()=>{console.log(taskValue)})
+
+
