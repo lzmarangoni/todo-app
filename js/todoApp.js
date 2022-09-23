425b9751dcf2e1242cc24cb231b979d9041ad9ca
Luiz Marangoni <lzmarangoni@gmail.com>
2022-09-23 19:04:29 -0300

    implementada funcionalidade de criar tarefas

--- js/todoApp.js
+++ js/todoApp.js
@@ -1,8 +1,61 @@
-const task = document.getElementById('todoInput');
-const taskValue = task.value;
-const addTask = document.querySelector('.todoCheck');
+const addTask = document.getElementById('newTask')
+const todoInput = document.getElementById('todoInput')
+const taskDone = document.getElementById('taskDone')
+const taskDelete = document.getElementById('taskDelete')
+const todoList = document.querySelector('.todoList')
+
+addTask.addEventListener('click',()=>{
+    let task = {
+        nome: todoInput.value,
+        id: randomId()
+    }
+    createTask(task)
+    console.log(task)
+    todoInput.value =''
+})
+
+function randomId(){
+    return Math.floor(Math.random() *1000)
+}
+
+function createTask(task){
+    let li = newLi(task)
+    todoList.appendChild(li)
+}
+
+function newLi(task){
+    let li = document.createElement('li')
+    li.classList.add("todoItem")
+
+    let div = document.createElement('div')
+    
+    
+    let input = document.createElement('input')
+    
+    let label = document.createElement('label')
+    div.classList.add("checkCustom")
+    let span = document.createElement('span')
+    span.innerHTML = task.nome
+    
+    let deleteButton = document.createElement('button')
+    deleteButton.classList.add('btnDelete')
+    deleteButton.setAttribute('onclick', 'deleteTask('+task.id+')')
+
+  
 
 
-addTask.addEventListener('click', ()=>{console.log(taskValue)})
+    
+    li.appendChild(div)
+    div.appendChild(input)
+    div.appendChild(label)
+    li.appendChild(span)
+    li.appendChild(deleteButton)
+
+    return li
+}
+
+function deleteTask(taskID){
+    console.log(taskID)
+}
 
 
